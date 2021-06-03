0bd2b69d150c5c7ccf6430c2ea1b856da7495915
siva8248 <79700405+siva8248@users.noreply.github.com>
2021-06-03 22:39:11 +0530

    Update Food.js

--- Food.js
+++ Food.js
@@ -1,6 +1,6 @@
 class Food {
     constructor(){
-        this.image = loadImage("images/Milk.png");
+        this.image = loadImage("Milk.png");
         this.lastFed;
         this.foodStock = 20;
     }
